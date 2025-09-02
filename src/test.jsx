fef32bb5ad3e9d9ad0cc579a298d0830b219ac9c
NaaluZeroNaalu <ajithruflex1210@gmail.com>
2025-09-02 11:16:25 +0530

    Removed files

--- src/test.jsx
+++ src/test.jsx
@@ -3,30 +3,11 @@ import { useEffect, useState } from "react";
 
 function Test() {
 
-  const [datas, Getdatas] = useState([])
-  
-  useEffect(()=>{
-
-    fetch("http://localhost:8000/collection/all")
-    .then(res => res.json())
-    .then(data => Getdatas(data.data))
-    .catch(err => console.log(err))
-  },[])
+ 
 
   return(
     <>
-    {
-  datas.map((val, index) => (
-    <div key={index}>
-      <p className="text-red-800">{val.payload.text}</p>
-      <audio 
-        src={`data:audio/mp3;base64,${val.payload.audio_base64}`} 
-        controls
-      ></audio>
-    </div>
-  ))
-}
-
+    
     </>
   )
 }
